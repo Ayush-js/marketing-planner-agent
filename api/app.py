7c65053ab2614a17083b8bceced6bec4daa91107
Ayush-js <ayushmishraamlai34@gmail.com>
2026-04-04 17:05:56 +0530

    Add Vercel URL to CORS allowed origins

--- api/app.py
+++ api/app.py
@@ -29,6 +29,7 @@ app.add_middleware(
         "http://127.0.0.1:5173",
         "http://localhost:3000",
         "http://127.0.0.1:3000",
+        "https://marketing-planner-agent.vercel.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
